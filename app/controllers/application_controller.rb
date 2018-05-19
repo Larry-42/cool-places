d35f7cdb7bbe7c9760af15772ec710d599645807
Larry <github email address>
2018-05-19 14:46:43 -0400

    Stubbed out signup page, added code to set Views directory

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < Sinatra::Base
+  
+  set :views, Proc.new { File.join(root, "../views/")}
+  
   get '/' do
     "Welcome to cool places!"
   end
+  
+  get '/signup' do
+    erb :signup
+  end
+  
+  post '/signup' do
+    #TODO:  Add signup
+  end
+
 end
