63a62cc099f33e109f99c1e7819a96f5469ddec8
Larry <github email address>
2018-05-19 15:09:17 -0400

    Added user creation with validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,27 @@ class ApplicationController < Sinatra::Base
   end
   
   post '/signup' do
-    #TODO:  ADD LOGIC
+    #Validation--Make sure that no parameters are empty
+    params[:user].each do |k, v|
+      if v.empty?
+        redirect to '/signup'
+      end
+    end
+    
+    #Validation--Make sure that password is at least 5 characters long
+    if params[:user][:password].length < 5
+      redirect to '/signup'
+    end
+    
+    #Validation--Make sure that username is not already taken
+    if User.find_by username: params[:user][:username]
+      redirect to '/signup'
+    end
+  
+    User.create(params[:user])
+    
+    #TODO: Set session, redirect to show places
+    "Successfully signed up"
   end
 
 end
