5e860337fd8212c6f244aa9fbbe5b1b1c4eacd88
Larry <github email address>
2018-05-19 13:46:56 -0400

    Added content to Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_05_19_170420) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "place_id"
+    t.text "content"
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.string "location"
+    t.text "content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
 
 end
